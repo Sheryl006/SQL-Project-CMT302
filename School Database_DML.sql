55f36d5bb6b1b6cfae7d6c75c463baf9cc4ce244
Ogada <157451005+Sheryl006@users.noreply.github.com>
2025-10-07 15:01:36 +0400

    Update School Database_DML.sql

--- School Database_DML.sql
+++ School Database_DML.sql
@@ -19,4 +19,31 @@ insert into Students(first_name,last_name,gender,dob,program_id,admission_year) 
 ('Caroline','Esther','F','2002-01-19',3,2023),
 ('Dennis','Adika','M','2005-12-24',2,2024);
 
-insert into Courses()
+insert into Courses(course_code ,course_name ,credit_hours ,program_id ,lecturer_id) values
+('CS101', 'Introduction to Computer Science', 3, 1, 1),
+('MATH201', 'Calculus II', 4, 2, 2),
+('ENG150', 'Database Systems', 2, 1, 1),
+('PHY110', 'General Physics', 3, 3, 1),
+('HIST210', 'World History', 3, 2, 3);
+
+insert into Enrollments(student_id,course_id,semester,grade) values
+(1, 1, 'Jan-Apr', 3.8),
+(1, 2, 'May-AUg', 3.6),
+(2, 1, 'Jan-Apr', 3.2),
+(3, 3, 'Jan-Apr', 4.0),
+(4, 4, 'May-AUg',3.9 );
+
+INSERT INTO LecturerProfiles (lecturer_id, profile_data)
+VALUES
+(3, JSON_OBJECT(
+    'experience', 5,
+    'Specialization', 'Data Science',
+    'Research_Areas', JSON_ARRAY('Machine Learning', 'AI Ethics')
+)),
+(1, JSON_OBJECT(
+    'experience', 2,
+    'Specialization', 'Cybersecurity',
+    'Research_Areas', JSON_ARRAY('Network Security', 'Cryptography')
+));
+
+
